3dcf19c19f8407e55be0e711ddcb0882297a90fc
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-08-06 19:17:17 +0100

    create home service

---
+++ src/app/services/home.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Chat } from '../Chat';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  private apiUrl = 'http://localhost:3000/chats';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getChats(): Observable<Chat[]> {
+    return this.http.get<Chat[]>(this.apiUrl);
+  }
+
+  updateChat(chat: Chat): Observable<any> {
+    return this.http.put(this.apiUrl, chat);
+  }
+
+  addChat(chat: Chat): Observable<Chat> {
+    return this.http.post<Chat>(this.apiUrl, chat);
+  }
+
+  deleteChat(chat: Chat | number): Observable<Chat> {
+    const id = typeof chat === 'number' ? chat : chat.id;
+    const url = `${this.apiUrl}/${id}`;
+
+    return this.http.delete<Chat>(url);
+  }
+
+
+}
