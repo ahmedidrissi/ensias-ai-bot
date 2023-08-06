1970529cd065acb7ce4ded1a779f95dc4394abcc
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-08-06 19:17:08 +0100

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Message } from '../message/Message';
 import { Chat } from '../../Chat';
-import { CHATS } from '../../mock-chats';
+import { HomeService } from '../../services/home.service';
 
 @Component({
   selector: 'app-home',
@@ -12,43 +12,51 @@ export class HomeComponent implements OnInit {
 
   chats: Chat[] = [];
   names : string[] = [];
+  currentChat: Chat = {
+    id: 0,
+    name: "",
+    messages: []
+  };
   currentName: string = "";
-  messages: Message[] = [];
+  currentMessages: Message[] = [];
   typingDots: Message = new Message(
     'incoming',
     'assets/chatgpt-icon.svg',
     'Bot',
     '',
     true
-  )
+  );
 
-  constructor() {}
+  constructor(
+    private homeService: HomeService
+  ) {}
 
   ngOnInit(): void {
-    this.chats = this.getChats();
-    this.chats.forEach(chat => {
-      this.names.push(chat.name);
+    this.homeService.getChats().subscribe(chats => {
+      this.chats = chats;
+      this.names = this.chats.map(chat => chat.name);
+      this.currentChat = chats[0];
+      this.currentName = this.names[0];
+      this.openChat(this.currentName);
     });
-    this.currentName = this.chats[0].name;
-    this.messages = this.chats[0].messages;
   }
 
   sendMessage() {
     var message = (<HTMLInputElement>document.getElementById("input")).value.trim();
     (<HTMLInputElement>document.getElementById("input")).value = "";
     if (message.length > 1) {
-      this.messages.push(new Message(
+      this.currentMessages.push(new Message(
         'outgoing',
         'https://img.icons8.com/material-rounded/35/ffffff/user.png',
         'User',
         message
       ));
       setTimeout(() => {
-        this.messages.push(this.typingDots);
+        this.currentMessages.push(this.typingDots);
       }, 1000);
       setTimeout(() => {
-        this.messages.pop();
-        this.messages.push(new Message(
+        this.currentMessages.pop();
+        this.currentMessages.push(new Message(
           'incoming',
           'assets/chatgpt-icon.svg',
           'Bot',
@@ -60,28 +68,18 @@ export class HomeComponent implements OnInit {
   }
 
   openChat(chatName: string) {
-    this.messages = this.chats.find(chat => chat.name === chatName)!.messages;
+    this.currentName = chatName;
+    this.currentMessages = this.chats.find(chat => chat.name == chatName)!.messages;
   }
 
   openNewChat() {
 
   }
 
-  getChats(): Chat[] {
-    return CHATS;
-  }
-
-  // loadChat(chatName: string): Chat {
-  //   let chat = this.getChats().find(chat => chat.name === chatName);
-  //   return chat!;
-  // }
-
   saveChat() {
-    let chat = this.getChats().find(chat => chat.name === this.currentName);
-    if (chat) {
-      chat.messages = this.messages;
-    } else {
-      CHATS.push({ name: this.currentName, messages: this.messages });
-    }
+    this.currentChat.messages = this.currentMessages;
+    console.log(this.currentChat);
+    
+    this.homeService.updateChat(this.currentChat).subscribe();
   }
 }
